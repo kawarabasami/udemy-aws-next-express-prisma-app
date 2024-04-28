0fc4e2735fd456d43898a2fa708d94a53c5f3f09
m-nakamine <kawarabasami.dev@gmail.com>
2024-04-28 15:59:25 +0900

    add createTodo api

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import { PrismaClient } from '@prisma/client';
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+
 const prisma = new PrismaClient();
 
 const PORT = 8080;
@@ -9,6 +11,17 @@ const PORT = 8080;
 app.get('/allTodos', async (req, res) => {
   const allTodos = await prisma.todo.findMany();
   return res.send(allTodos);
+});
+
+app.post('/createTodo', async (req, res) => {
+  const { title, isCompleted } = req.body;
+  const newTodo = await prisma.todo.create({
+    data: {
+      title,
+      isCompleted
+    }
+  });
+  return res.send(newTodo);
 });
 
 app.listen(PORT, () => {
