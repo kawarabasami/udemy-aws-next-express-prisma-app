3c7bf7d0348d422756141569f66f52eeb2f71c00
m-nakamine <kawarabasami.dev@gmail.com>
2024-04-28 18:52:44 +0900

    add completed check

--- client/src/app/components/Todo.tsx
+++ client/src/app/components/Todo.tsx
@@ -52,6 +52,25 @@ function Todo({ todo }: TodoProps) {
     setEditedTitle("");
   };
 
+  const toggleTodoCompletion = async (id: number, isCompleted: boolean) => {
+    const response = await fetch(`http://localhost:8080/editTodo/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        isCompleted: !isCompleted,
+      }),
+    });
+
+    if (response.ok) {
+      const editedTodo = await response.json();
+      const updatedTodos = todos.map((t) =>
+        t.id === editedTodo.id ? editedTodo : t
+      );
+      mutate([...updatedTodos]);
+      setEditedTitle("");
+    }
+  };
+
   return (
     <li className="py-4">
       <div className="flex items-center justify-between">
@@ -62,6 +81,8 @@ function Todo({ todo }: TodoProps) {
             type="checkbox"
             className="h-4 w-4 text-teal-600 focus:ring-teal-500
                   border-gray-300 rounded"
+            checked={todo.isCompleted}
+            onChange={() => toggleTodoCompletion(todo.id, todo.isCompleted)}
           />
           <label className="ml-3 block text-gray-900">
             {isEditing ? (
@@ -72,11 +93,17 @@ function Todo({ todo }: TodoProps) {
                 onChange={(e) => setEditedTitle(e.target.value)}
               />
             ) : (
-              <span className="text-lg font-medium mr-2"> {todo.title} </span>
+              <span
+                className={`text-lg font-medium mr-2 ${
+                  todo.isCompleted ? "line-through" : ""
+                }`}
+              >
+                {todo.title}
+              </span>
             )}
           </label>
         </div>
-        <div className="flex items-center space-x-2">
+        <div className={`flex items-center space-x-2 `}>
           <button
             className="duration-150 bg-green-600 hover:bg-green-700 text-white font-medium py-1 px-2 rounded"
             onClick={handleEdit}
