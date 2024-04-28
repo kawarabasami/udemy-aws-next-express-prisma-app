c358d01963ca26eb6a7f15ac093cc0689f5403cf
m-nakamine <kawarabasami.dev@gmail.com>
2024-04-28 15:39:17 +0900

    add allTodos endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,13 @@
-console.log('hello express');
+import express from 'express';
+
+const app = express();
+
+const PORT = 8080;
+
+app.get('/allTodos', (req, res) => {
+  return res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
